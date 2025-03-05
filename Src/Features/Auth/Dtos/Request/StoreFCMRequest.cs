8b57b8e7ebc527981db56a736d272889080b6361
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-03-05 15:41:51 +0700

    hotfix fcmtoken update

--- Src/Features/Auth/Dtos/Request/StoreFCMRequest.cs
+++ Src/Features/Auth/Dtos/Request/StoreFCMRequest.cs
@@ -6,7 +6,6 @@ namespace HUBT_Social_API.Src.Features.Auth.Dtos.Request
     public class StoreFCMRequest
     {
         [Required(ErrorMessage = "FcmToken không được để trống.")]
-        [RegularExpression(@"^([a-zA-Z0-9\-\_:]{100,})$", ErrorMessage = "FcmToken không hợp lệ.")]
         public string FcmToken { get; set; } = string.Empty;
     }
 }
